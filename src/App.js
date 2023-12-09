8b4dce08b44706a2748998de0ccc75ec690400b5
jhantai <tylerbroceva123@gmail.com>
2023-12-09 11:00:28 +0600

    First commin

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import './App.css'; 
+import GetImgs from './pages/imgs/imgs';
+import Character from './pages/character/character';
+import DetailedPage from './pages/detailedPage/detailedPage';
+
+import { Route, Routes } from 'react-router-dom';
+
 
 function App() {
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Routes>
+      <Route exact path='/' element={<GetImgs/>}/>
+      <Route exact path='/Character' element={<Character/>}/>
+      <Route exact path='/hero/:id' element={<DetailedPage/>}/>
+    </Routes>
     </div>
   );
 }
---
+++ src/pages/imgs/imgs.jsx
@@ -0,0 +1,18 @@
+
+import './imgs.css';
+
+
+const GetImgs = () => {
+     
+  return (
+    <div className='body'>
+         <a href="/character"><input type="button" value="Игра Престолов "   className='GetImgs-button'/></a>
+        
+        
+         
+        
+    </div>
+  )
+}
+
+export default GetImgs
